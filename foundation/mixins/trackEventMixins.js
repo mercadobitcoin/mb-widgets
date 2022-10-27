4cd8b03914d9b3e51a93ca235bcbf96540514cae
Kennedy Oliveira <kennedy.oliveira@mercadobitcoin.com.br>
2022-10-27 18:04:59 -0400

    Add new mixin to trackEvents

---
+++ foundation/mixins/trackEventMixins.js
@@ -0,0 +1,15 @@
+window.MB_WIDGETS = window.MB_WIDGETS || {}
+window.MB_WIDGETS.trackEvent = {//eslint-disable-line
+  methods: {
+    ga: (event) => {
+      try {
+        if (gtag) { //eslint-disable-line
+          gtag('event', event.en, { //eslint-disable-line
+            event_category: event.ec,
+            event_label: event.lb
+          })
+        }
+      } catch (e) {}
+    }
+  }
+}
