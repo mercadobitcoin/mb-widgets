1c57a04b5d48dc540fa4e0cf743218754f7f6203
leonardokato-mb <leonardo.kato@mercadobitcoin.com.br>
2022-11-29 09:58:08 -0300

    pre commit

--- widgets/mbwd-fixed-income-simulator/components/js/mbwd-fixed-income-simulator.js
+++ widgets/mbwd-fixed-income-simulator/components/js/mbwd-fixed-income-simulator.js
@@ -1,10 +1,11 @@
 const MBWD_FIXED_INCOME_SIMULATOR = () => ({ //eslint-disable-line
   template: `
     <div class="mbwd-fixed-income-simulator">
-      <div class="interactive-components-wrapper">
-        <h2 class="main-title">Veja quanto o seu dinheiro pode render com ENER02.</h2>
-        <div>
-          <h3 class="auxiliar-title">Qual valor você gostaria de investir?</h3>
+      <h2 class="main-title">Simulador de Renda Fixa Digital</h2>
+      <p class="main-subtitle">Veja quanto o seu dinheiro pode render com ENER02.</p>
+      <div class="main-wrapper">
+        <div class="interactive-components-wrapper">
+          <h3 class="auxiliar-title">Quanto você gostaria de investir?</h3>
           <p class="auxiliar-subtitle">O investimento inicial em Renda Fixa Digital é de R$ 100.</p>
           <div>
             <input></input>
@@ -17,42 +18,42 @@ const MBWD_FIXED_INCOME_SIMULATOR = () => ({ //eslint-disable-line
           <p>{{estimatedReturnAmount}}</p>
           <a :href=basicExperiencePairTradingUrl>Quero Investir</a>
         </div>
-      </div>
-      <div class="comparison-components-wrapper">
-        <div>
-          <h3>Prazo estimado de pagamento: dezembro/2022</h3>
+        <div class="comparison-components-wrapper">
           <div>
-            <p v-if="asset.partialExpiration">Como este ativo possui liquidações parciais, você receberá remunerações fracionadas até o vencimento do investimento.</p>
-            <p v-else> Data prevista para o recebimento da rentabilidade total do ativo.</p>
-            <p>Para mais detalhes, acesse a <a :href="asset.detailsPdfUrl">lâmina do ativo</a>.</p>
-          </div>
-        </div>
-
-        <div class="comparison-graph-bar-wrapper">
-          <h3>Comparativo com outros investimentos</h3>
-          <div class="bars-wrapper">
-
-            <div class="graph-bar-item">
-              <div class="bar-container">
-                <i class="bar-filler"> </i>
-              </div>
-              <p>{{ asset.displayName }}</p>
-              <p>{{ estimatedReturnAmount }}</p>
-              <p>{{ asset.estimatedYieldLabel }}</p>
+            <h3>Prazo estimado de pagamento: dezembro/2022</h3>
+            <div>
+              <p v-if="asset.partialExpiration">Como este ativo possui liquidações parciais, você receberá remunerações fracionadas até o vencimento do investimento.</p>
+              <p v-else> Data prevista para o recebimento da rentabilidade total do ativo.</p>
+              <p>Para mais detalhes, acesse a <a :href="asset.detailsPdfUrl">lâmina do ativo</a>.</p>
             </div>
-            
-            <div class="graph-bar-item">
-              <div class="bar-container">
-                <i class="bar-filler"> </i>
-              </div>
-              <p>{{ investmentAssetsComparisonItem01.displayName }}</p>
-              <p>{{ estimatedReturnAmount }}</p>
-              <p>{{ investmentAssetsComparisonItem01.estimatedYieldLabel }}</p>
-            </div>
-
           </div>
-        </div>
 
+          <div class="comparison-graph-bar-wrapper">
+            <h3>Comparativo com outros investimentos</h3>
+            <div class="bars-wrapper">
+
+              <div class="graph-bar-item">
+                <div class="bar-container">
+                  <i class="bar-filler"> </i>
+                </div>
+                <p>{{ asset.displayName }}</p>
+                <p>{{ estimatedReturnAmount }}</p>
+                <p>{{ asset.estimatedYieldLabel }}</p>
+              </div>
+              
+              <div class="graph-bar-item">
+                <div class="bar-container">
+                  <i class="bar-filler"> </i>
+                </div>
+                <p>{{ investmentAssetsComparisonItem01.displayName }}</p>
+                <p>{{ estimatedReturnAmount }}</p>
+                <p>{{ investmentAssetsComparisonItem01.estimatedYieldLabel }}</p>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   `,
